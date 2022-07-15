1967000448b7f70605e492ec7b62a12722f1f09c
Kevin Bui <kvb2univpitt@gmail.com>
2022-07-15 08:03:15 -0600

    Added REST service initial layout.

---
+++ sharephe-ws/src/main/java/edu/pitt/dbmi/sharephe/ws/SharepheWsApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.sharephe.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ *
+ * Jul 14, 2022 8:46:00 PM
+ *
+ * @author Kevin V. Bui (kvb2univpitt@gmail.com)
+ */
+@SpringBootApplication
+public class SharepheWsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SharepheWsApplication.class);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SharepheWsApplication.class, args);
+    }
+
+}
